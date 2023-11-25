0463ba8ce8089614fd40da7380abb2c0b79d1e4d
cahidenes <cahideneskeles54@gmail.com>
2023-11-25 11:32:18 +0300

    .

--- weeks/week2/q8/sol.py
+++ weeks/week2/q8/sol.py
@@ -1,1 +1,15 @@
-print(input())
+a = int(input())
+
+k = 1
+while k < a:
+    k *= 2
+
+kuvvetler = []
+while k > 0:
+    if a >= k:
+        kuvvetler.append(k)
+        a -= k
+    k //= 2
+
+print(*reversed(kuvvetler), sep=" + ")
+
