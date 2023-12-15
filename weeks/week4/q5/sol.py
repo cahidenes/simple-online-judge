bbc92aaacd4415e51ccc6771c7605a3b34ac935c
cahidenes <cahideneskeles54@gmail.com>
2023-12-15 15:08:02 +0300

    .

--- weeks/week4/q5/sol.py
+++ weeks/week4/q5/sol.py
@@ -2,6 +2,6 @@ import cowsay
 
 karakter = input()
 if karakter in cowsay.char_names:
-    cowsay.chars[karakter]('SA, Python çok güzel')
+    cowsay.char_funcs[karakter]('SA, Python çok güzel')
 else:
     print('YOK')
