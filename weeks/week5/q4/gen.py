4775f2bf06a47425111a56a9128be0282ced8a4e
cahidenes <cahideneskeles54@gmail.com>
2023-12-31 01:48:38 +0300

    .

--- weeks/week5/q4/gen.py
+++ weeks/week5/q4/gen.py
@@ -1,20 +1,20 @@
 import random
-print('4 4')
-mat = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-for i in range(4):
-    for j in range(4):
+print('3 3')
+mat = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+for i in range(3):
+    for j in range(3):
         if random.randint(0, 1) == 1:
             mat[i][j] ^= 1
             if i > 0:
                 mat[i - 1][j] ^= 1
-            if i < 3:
+            if i < 2:
                 mat[i + 1][j] ^= 1
             if j > 0:
                 mat[i][j - 1] ^= 1
-            if j < 3:
+            if j < 2:
                 mat[i][j + 1] ^= 1
-for i in range(4):
-    for j in range(4):
+for i in range(3):
+    for j in range(3):
         print(mat[i][j], end=' ')
     print()
 
