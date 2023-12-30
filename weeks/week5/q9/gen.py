eb9e38e4764b0238cc85c93821059627d03d9310
cahidenes <cahideneskeles54@gmail.com>
2023-12-31 01:49:32 +0300

    .

--- weeks/week5/q9/gen.py
+++ weeks/week5/q9/gen.py
@@ -1,20 +1,20 @@
 import random
-print('10 10')
-mat = [[0 for i in range(10)] for j in range(10)]
-for i in range(10):
-    for j in range(10):
+print('9 9')
+mat = [[0 for i in range(9)] for j in range(9)]
+for i in range(9):
+    for j in range(9):
         if random.randint(0, 1) == 1:
             mat[i][j] ^= 1
             if i > 0:
                 mat[i - 1][j] ^= 1
-            if i < 9:
+            if i < 8:
                 mat[i + 1][j] ^= 1
             if j > 0:
                 mat[i][j - 1] ^= 1
-            if j < 9:
+            if j < 8:
                 mat[i][j + 1] ^= 1
-for i in range(10):
-    for j in range(10):
+for i in range(9):
+    for j in range(9):
         print(mat[i][j], end=' ')
     print()
 
