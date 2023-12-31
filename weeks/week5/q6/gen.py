3c5484813c5c441ee31b74254552a98d2a2e62d5
cahidenes <cahideneskeles54@gmail.com>
2023-12-31 21:35:45 +0300

    .

--- weeks/week5/q6/gen.py
+++ weeks/week5/q6/gen.py
@@ -4,6 +4,6 @@ n = random.randint(3, 8)
 m = random.randint(5, 15)
 print(n, m)
 print(*random.sample(['Boy', 'Genişlik', 'En', 'Kalınlık', 'Yaş', 'Sağlık', 'Verimlilik', 'Yararlılık', 'Mesafe', 'Yaşam', 'Derinlik', 'Yoğunluk'], n))
-for i in range(m-1):
+for i in range(m):
     print(*[random.randint(1, 10000) for _ in range(n)])
 
