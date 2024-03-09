c25577b3dac8854514d60799e881c0867ff9e276
cahidenes <cahideneskeles54@gmail.com>
2024-03-09 16:16:34 +0300

    .

--- weeks/week1/q3/gen.py
+++
@@ -1,9 +0,0 @@
-import random
-
-n = random.randint(3, 8)
-m = random.randint(5, 15)
-print(n, m)
-print(*random.sample(['Boy', 'Genişlik', 'En', 'Kalınlık', 'Yaş', 'Sağlık', 'Verimlilik', 'Yararlılık', 'Mesafe', 'Yaşam', 'Derinlik', 'Yoğunluk'], n))
-for i in range(m):
-    print(*[random.randint(1, 10000) for _ in range(n)])
-
